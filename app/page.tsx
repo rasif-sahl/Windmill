f5edab418bfc6b76d2e928622c8e3bc96a028b26
rasif-sahl <rasifjunaid2@gmail.com>
2024-10-14 21:40:17 +0530

    code editor added

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import CodeEditor from '@/components/codeEditor';
 import Layout from '../components/Layout';
 
 const links = [
@@ -11,16 +12,26 @@ const links = [
   },
 ]
 
+const exampleCode = `
+import React from 'react';
+
+function Example() {
+  return <div>Hello, World!</div>;
+}
+
+export default Example;
+`
 
 export default function Home() {
   return (
     <Layout asideList={links}>
       <section>
         <h2 className="text-3xl font-semibold mb-4">Introduction</h2>
-        <p className="text-para-color">
+        <p className="text-para-color mb-4">
           Agon is a Laravel Multipurpose Agency script using Laravel 10.x. It’s a product built on Botble CMS.
         </p>
         {/* Add more content here */}
+        <CodeEditor code={exampleCode}/>
       </section>
     </Layout>
   );
